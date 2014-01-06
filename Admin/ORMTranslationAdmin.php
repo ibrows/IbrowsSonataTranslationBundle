b72d7ba99bd0e2537817c49efdc88f81c6d29ba7
Gladhon <marc.steiner@ibrows.ch>
2014-01-06 16:08:20 +0100

    add content filter

--- Admin/ORMTranslationAdmin.php
+++ Admin/ORMTranslationAdmin.php
@@ -11,6 +11,40 @@ class ORMTranslationAdmin extends TranslationAdmin
         $filter
             ->add('key', 'doctrine_orm_string')
             ->add('domain', 'doctrine_orm_string')
+            ->add('content', 'doctrine_orm_callback',
+                array
+                (
+                    'callback' => function(\Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery $queryBuilder, $alias, $field, $options)
+                    {
+                        /* @var $queryBuilder Sonata\AdminBundle\Datagrid\ORM\ProxyQuery */
+                        if(!isset($options['value']) || !$options['value'])
+                        {
+                            return;
+                        }
+                        $value = $options['value'];
+
+                        $allreadejoind=false;
+                        $joins = $queryBuilder->getDQLPart('join');
+                        if(array_key_exists($alias, $joins)){
+                            $joins = $joins[$alias];
+                            foreach($joins as $join){
+                                if(strpos($join->__toString(), "$alias.translations ")){
+                                    $allreadejoind = true;
+                                }
+                            }
+                        }
+                        if(!$allreadejoind){
+                            $queryBuilder->innerJoin(sprintf('%s.translations', $alias), 'translations');
+                        }
+                        $value = "%$value%";
+                        $queryBuilder->andWhere('translations.content LIKE :content');
+                        $queryBuilder->setParameter('content', $value);
+//                        $queryBuilder->andWhere($queryBuilder->expr()->in('translations.content', $value));
+                    },
+                    'field_type' => 'text',
+                    'label' => 'content'
+                )
+            )
         ;
     }
 }
