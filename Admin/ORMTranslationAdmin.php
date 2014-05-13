102afbbb1f9fd697d161e1e2ca42a337f58f8ee1
Mike Meier <mike.meier@ibrows.ch>
2014-05-13 15:58:08 +0200

    fix domain array initial value

--- Admin/ORMTranslationAdmin.php
+++ Admin/ORMTranslationAdmin.php
@@ -3,19 +3,24 @@
 namespace Ibrows\SonataTranslationBundle\Admin;
 
 use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery;
 
 class ORMTranslationAdmin extends TranslationAdmin
 {
     protected function configureDatagridFilters(DatagridMapper $filter)
     {
-
-    	$em = $this->getConfigurationPool()->getContainer()->get('doctrine')->getEntityManagerForClass('Lexik\Bundle\TranslationBundle\Entity\File');
+    	$em = $this->getConfigurationPool()->getContainer()->get('doctrine')->getManagerForClass('Lexik\Bundle\TranslationBundle\Entity\File');
     	$repo = $em->getRepository('Lexik\Bundle\TranslationBundle\Entity\File');
+
+        $domains = array();
+
     	foreach($repo->findAll() as $file){
     		/* @var $file  \Lexik\Bundle\TranslationBundle\Entity\File */
     		$domains[$file->getDomain()] = $file->getDomain();
     	}
+
     	ksort($domains);
+
     	$filter
 	    	->add('key', 'doctrine_orm_string')
 	    	->add('domain', 'doctrine_orm_choice', array(
@@ -32,9 +37,9 @@ class ORMTranslationAdmin extends TranslationAdmin
             ->add('content', 'doctrine_orm_callback',
                 array
                 (
-                    'callback' => function(\Sonata\DoctrineORMAdminBundle\Datagrid\ProxyQuery $queryBuilder, $alias, $field, $options)
+                    'callback' => function(ProxyQuery $queryBuilder, $alias, $field, $options)
                     {
-                        /* @var $queryBuilder Sonata\AdminBundle\Datagrid\ORM\ProxyQuery */
+                        /* @var $queryBuilder ProxyQuery */
                         if(!isset($options['value']) || !$options['value'])
                         {
                             return;
